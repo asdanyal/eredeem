6573f2f08eb7a83cc9bdc888acc58d6bea5206c9
ASDanyal <12bscsadanyal@seecs.edu.pk>
2018-12-08 19:20:44 +0500

    [UPDATE] USER SERVICE Mobile number removed

---
+++ User Service/src/main/java/com/skillicious/response/Message.java
@@ -0,0 +1,18 @@
+package com.skillicious.response;
+
+public class Message {
+
+    private String details;
+
+    public Message(String details) {
+        this.details = details;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+}
