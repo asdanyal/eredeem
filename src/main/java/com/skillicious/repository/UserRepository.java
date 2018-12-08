0a13381444dde5b07ca274f88651ec6333112414
ASDanyal <12bscsadanyal@seecs.edu.pk>
2018-12-08 19:15:54 +0500

    [UPDATE] USERS API ADDED

--- src/main/java/com/skillicious/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.skillicious.repository;
-
-import com.skillicious.domain.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-
-}
